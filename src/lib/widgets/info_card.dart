b5b9efc30d21dce81f4b0442bbf9bd068f156b06
Ibrahim Mammadov <zxyctn@gmail.com>
2021-03-09 18:17:47 +0100

    Bug fixes, performance optimization

--- src/lib/widgets/info_card.dart
+++ src/lib/widgets/info_card.dart
@@ -21,197 +21,201 @@ class InfoCard extends StatelessWidget {
         height: double.infinity,
         padding: const EdgeInsets.all(15),
         color: Color.fromRGBO(110, 220, 255, 1),
-        child: _width > 700 ? Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            CircleAvatar(
-              backgroundImage: AssetImage('assets/images/profile.jpeg'),
-              backgroundColor: Color.fromRGBO(110, 220, 255, 1),
-              radius: 0.1 * _height,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Ibrahim Mammadov',
-                  style: Theme.of(context).textTheme.button.copyWith(
-                        fontSize: 0.04 * _height,
-                      ),
-                ),
-                Text(
-                  'Master\'s student at',
-                  style: Theme.of(context).textTheme.button.copyWith(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 30 / 1080 * _height,
-                      ),
-                ),
-                Row(
-                  children: [
-                    Text(
-                      'University of ',
-                      style: Theme.of(context).textTheme.button.copyWith(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 30 / 1080 * _height,
-                          ),
-                    ),
-                    Text(
-                      'Weimar',
-                      style: Theme.of(context).textTheme.button.copyWith(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 30 / 1080 * _height,
-                            color: Color.fromRGBO(119, 0, 255, 1),
-                          ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20 / 1080 * _height,
-                ),
-                Container(
-                  child: Row(
+        child: _width > 700
+            ? Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/images/profile.jpeg'),
+                    backgroundColor: Color.fromRGBO(110, 220, 255, 1),
+                    radius: 0.1 * _height,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Container(
-                        width: 0.04 * _height,
-                        child: FlatButton(
-                          child: Image.asset(
-                            'assets/images/linkedin.png',
-                            height: 0.04 * _height,
-                            width: 0.04 * _height,
-                            fit: BoxFit.cover,
+                      Text(
+                        'Ibrahim Mammadov',
+                        style: Theme.of(context).textTheme.button.copyWith(
+                              fontSize: 0.04 * _height,
+                            ),
+                      ),
+                      Text(
+                        'Master\'s student at',
+                        style: Theme.of(context).textTheme.button.copyWith(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 30 / 1080 * _height,
+                            ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'University of ',
+                            style: Theme.of(context).textTheme.button.copyWith(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 30 / 1080 * _height,
+                                ),
                           ),
-                          padding: EdgeInsets.all(0),
-                          visualDensity: VisualDensity.compact,
-                          materialTapTargetSize:
-                              MaterialTapTargetSize.shrinkWrap,
-                          onPressed: () {
-                            _launchURL('https://www.linkedin.com/in/zxyctn');
-                          },
-                        ),
+                          Text(
+                            'Weimar',
+                            style: Theme.of(context).textTheme.button.copyWith(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 30 / 1080 * _height,
+                                  color: Color.fromRGBO(119, 0, 255, 1),
+                                ),
+                          ),
+                        ],
                       ),
                       SizedBox(
-                        width: 20 / 1920 * _width,
+                        height: 20 / 1080 * _height,
                       ),
                       Container(
-                        width: 0.04 * _height,
-                        child: FlatButton(
-                          child: Image.asset(
-                            'assets/images/github.png',
-                            width: 0.04 * _height,
-                            height: 0.04 * _height,
-                            fit: BoxFit.cover,
-                          ),
-                          padding: EdgeInsets.all(0),
-                          visualDensity: VisualDensity.compact,
-                          materialTapTargetSize:
-                              MaterialTapTargetSize.shrinkWrap,
-                          onPressed: () {
-                            _launchURL('http://github.com/zxyctn');
-                          },
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 0.04 * _height,
+                              child: FlatButton(
+                                child: Image.asset(
+                                  'assets/images/linkedin.png',
+                                  height: 0.04 * _height,
+                                  width: 0.04 * _height,
+                                  fit: BoxFit.cover,
+                                ),
+                                padding: EdgeInsets.all(0),
+                                visualDensity: VisualDensity.compact,
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                                onPressed: () {
+                                  _launchURL(
+                                      'https://www.linkedin.com/in/zxyctn');
+                                },
+                              ),
+                            ),
+                            SizedBox(
+                              width: 20 / 1920 * _width,
+                            ),
+                            Container(
+                              width: 0.04 * _height,
+                              child: FlatButton(
+                                child: Image.asset(
+                                  'assets/images/github.png',
+                                  width: 0.04 * _height,
+                                  height: 0.04 * _height,
+                                  fit: BoxFit.cover,
+                                ),
+                                padding: EdgeInsets.all(0),
+                                visualDensity: VisualDensity.compact,
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                                onPressed: () {
+                                  _launchURL('http://github.com/zxyctn');
+                                },
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
-                ),
-              ],
-            ),
-          ],
-        ) : Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            CircleAvatar(
-              backgroundImage: AssetImage('assets/images/profile.jpeg'),
-              backgroundColor: Color.fromRGBO(110, 220, 255, 1),
-              radius: 0.08 * _height,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Ibrahim Mammadov',
-                  style: Theme.of(context).textTheme.button.copyWith(
-                        fontSize: 0.04 * _height,
-                      ),
-                ),
-                Text(
-                  'Master\'s student at',
-                  style: Theme.of(context).textTheme.button.copyWith(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 30 / 1080 * _height,
-                      ),
-                ),
-                Row(
-                  children: [
-                    Text(
-                      'University of ',
-                      style: Theme.of(context).textTheme.button.copyWith(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 30 / 1080 * _height,
-                          ),
-                    ),
-                    Text(
-                      'Weimar',
-                      style: Theme.of(context).textTheme.button.copyWith(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 30 / 1080 * _height,
-                            color: Color.fromRGBO(119, 0, 255, 1),
-                          ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20 / 1080 * _height,
-                ),
-                Container(
-                  child: Row(
+                ],
+              )
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/images/profile.jpeg'),
+                    backgroundColor: Color.fromRGBO(110, 220, 255, 1),
+                    radius: 0.08 * _height,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Container(
-                        width: 0.04 * _height,
-                        child: FlatButton(
-                          child: Image.asset(
-                            'assets/images/linkedin.png',
-                            height: 0.04 * _height,
-                            width: 0.04 * _height,
-                            fit: BoxFit.cover,
+                      Text(
+                        'Ibrahim Mammadov',
+                        style: Theme.of(context).textTheme.button.copyWith(
+                              fontSize: 0.04 * _height,
+                            ),
+                      ),
+                      Text(
+                        'Master\'s student at',
+                        style: Theme.of(context).textTheme.button.copyWith(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 30 / 1080 * _height,
+                            ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'University of ',
+                            style: Theme.of(context).textTheme.button.copyWith(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 30 / 1080 * _height,
+                                ),
                           ),
-                          padding: EdgeInsets.all(0),
-                          visualDensity: VisualDensity.compact,
-                          materialTapTargetSize:
-                              MaterialTapTargetSize.shrinkWrap,
-                          onPressed: () {
-                            _launchURL('https://www.linkedin.com/in/zxyctn');
-                          },
-                        ),
+                          Text(
+                            'Weimar',
+                            style: Theme.of(context).textTheme.button.copyWith(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 30 / 1080 * _height,
+                                  color: Color.fromRGBO(119, 0, 255, 1),
+                                ),
+                          ),
+                        ],
                       ),
                       SizedBox(
-                        width: 20 / 1920 * _width,
+                        height: 20 / 1080 * _height,
                       ),
                       Container(
-                        width: 0.04 * _height,
-                        child: FlatButton(
-                          child: Image.asset(
-                            'assets/images/github.png',
-                            width: 0.04 * _height,
-                            height: 0.04 * _height,
-                            fit: BoxFit.cover,
-                          ),
-                          padding: EdgeInsets.all(0),
-                          visualDensity: VisualDensity.compact,
-                          materialTapTargetSize:
-                              MaterialTapTargetSize.shrinkWrap,
-                          onPressed: () {
-                            _launchURL('http://github.com/zxyctn');
-                          },
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 0.04 * _height,
+                              child: FlatButton(
+                                child: Image.asset(
+                                  'assets/images/linkedin.png',
+                                  height: 0.04 * _height,
+                                  width: 0.04 * _height,
+                                  fit: BoxFit.cover,
+                                ),
+                                padding: EdgeInsets.all(0),
+                                visualDensity: VisualDensity.compact,
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                                onPressed: () {
+                                  _launchURL(
+                                      'https://www.linkedin.com/in/zxyctn');
+                                },
+                              ),
+                            ),
+                            SizedBox(
+                              width: 20 / 1920 * _width,
+                            ),
+                            Container(
+                              width: 0.04 * _height,
+                              child: FlatButton(
+                                child: Image.asset(
+                                  'assets/images/github.png',
+                                  width: 0.04 * _height,
+                                  height: 0.04 * _height,
+                                  fit: BoxFit.cover,
+                                ),
+                                padding: EdgeInsets.all(0),
+                                visualDensity: VisualDensity.compact,
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                                onPressed: () {
+                                  _launchURL('http://github.com/zxyctn');
+                                },
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
-                ),
-              ],
-            ),
-          ],
-        ),
+                ],
+              ),
       ),
     );
   }
