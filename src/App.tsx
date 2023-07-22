4bb00f0f3e96d2480470e926819614bdec145991
Ibrahim Mammadov <zxyctn@gmail.com>
2023-07-22 21:48:20 +0200

    Add SearchBar into the Index

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import SearchBar from './components/SearchBar';
+
 const App = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <div className='h-screen relative'>
+      <div className='grid w-full items-center justify-center '>
+        <div className='grid gap-5 sm:mt-96'>
+          <h1 className='font-bold text-center transform-all duration-1000 text-6xl md:text-8xl text-primary'>
+            Hello
+          </h1>
+          <SearchBar />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
