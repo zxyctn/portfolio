1edd56897c90c02d21918ddb96af48adb1fccaab
Ibrahim Mammadov <zxyctn@gmail.com>
2023-07-22 21:47:52 +0200

    Add custom themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,35 @@ export default {
     extend: {},
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    darkTheme: 'dark',
+    themes: [
+      {
+        light: {
+          primary: '#3B4D8D',
+          secondary: '#DCE4FF',
+          accent: '#E137D0',
+          neutral: '#FFFFFF',
+          'base-100': '#FFFFFF',
+          info: '#3ABFF8',
+          success: '#36D399',
+          warning: '#FBBD23',
+          error: '#F87272',
+        },
+      },
+      {
+        dark: {
+          primary: '#3B4D8D',
+          secondary: '#DCE4FF',
+          accent: '#E137D0',
+          neutral: '#141724',
+          'base-100': '#000000',
+          info: '#3ABFF8',
+          success: '#36D399',
+          warning: '#FBBD23',
+          error: '#F87272',
+        },
+      },
+    ],
+  },
 };
