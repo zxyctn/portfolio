57122415269a408bf5b74eb944ebd0746ed129c8
Ibrahim Mammadov <zxyctn@gmail.com>
2023-07-27 21:53:25 +0200

    Add both pages and submit handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import SearchBar from './components/SearchBar';
+import { useState } from 'react';
+
+import Index from './pages/Index';
+import Search from './pages/Search';
 
 const App = () => {
-  return (
-    <div className='h-screen relative'>
-      <div className='grid w-full items-center justify-center '>
-        <div className='grid gap-5 sm:mt-96'>
-          <h1 className='font-bold text-center transform-all duration-1000 text-6xl md:text-8xl text-primary'>
-            Hello
-          </h1>
-          <SearchBar />
-        </div>
-      </div>
-    </div>
+  const [page, setPage] = useState('index');
+  const [search, setSearch] = useState('');
+
+  const handleSubmit = (q: string) => {
+    setPage('search');
+    setSearch(q);
+  };
+
+  return page === 'index' ? (
+    <Index handleSubmit={handleSubmit} />
+  ) : (
+    <Search q={search} handleSubmit={handleSubmit} />
   );
 };
 
