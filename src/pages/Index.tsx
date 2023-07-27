f1c3b9072b777033188104fb9c8c2de88aacfe9a
Ibrahim Mammadov <zxyctn@gmail.com>
2023-07-27 21:52:57 +0200

    Finish Index

---
+++ src/pages/Index.tsx
@@ -0,0 +1,19 @@
+import SearchBar from '../components/SearchBar';
+import type { SearchProps } from '../types';
+
+const Index = ({ handleSubmit }: SearchProps) => {
+  return (
+    <div className='h-screen relative'>
+      <div className='grid w-full items-center justify-center '>
+        <div className='grid gap-5 sm:mt-96'>
+          <h1 className='font-bold text-center transform-all duration-1000 text-6xl md:text-8xl text-primary'>
+            Hello
+          </h1>
+          <SearchBar value='' handleSubmit={handleSubmit} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
