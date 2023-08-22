36a303a596e9ea3681386fa4e320576553200130
Ibrahim Mammadov <zxyctn@gmail.com>
2023-08-22 22:02:01 +0200

    Add component props types

--- src/types.ts
+++ src/types.ts
@@ -9,3 +9,24 @@ export type SearchProps = {
   q?: string;
 	handleSubmit: (q: string) => void;
 };
+
+export type CardProps = {
+  header?: string;
+  content?: string;
+  footer?: string;
+  url?: string;
+  image?: string;
+};
+
+export type TabProps = {
+  id: string;
+  name: string;
+  active: boolean;
+  clickHandler: (id: string) => void;
+};
+
+export type LinkProps = {
+  href: string;
+  text: string;
+  platform: string;
+};
