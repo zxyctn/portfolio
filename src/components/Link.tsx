bf765eb624340a3484e869230e86ae88f5782f87
Ibrahim Mammadov <zxyctn@gmail.com>
2023-08-22 22:01:17 +0200

    Create Link

---
+++ src/components/Link.tsx
@@ -0,0 +1,27 @@
+import { Linkedin, Github } from 'react-bootstrap-icons';
+
+import { LinkProps } from '../types';
+
+const Link = ({ href, text, platform }: LinkProps) => {
+  return (
+    <div className='grid text-primary'>
+      <div className='flex gap-4 items-center'>
+        <div className='border-2 bg-secondary rounded-full w-14 h-14 flex items-center justify-center'>
+          {platform === 'LinkedIn' ? (
+            <Linkedin className='text-primary' size={24} />
+          ) : (
+            <Github className='text-primary' size={24} />
+          )}
+        </div>
+        <div>
+          <h1 className='text-xl'>{platform}</h1>
+          <a href={href}>{href}</a>
+        </div>
+      </div>
+      <h1 className='text-xl my-auto'>zxyctn</h1>
+      <p className='max-w-[600px]'>{text}</p>
+    </div>
+  );
+};
+
+export default Link;
