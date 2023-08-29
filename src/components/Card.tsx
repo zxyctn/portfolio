9f05416a40b6a39c79d1c05437d985442753abb0
Ibrahim Mammadov <zxyctn@gmail.com>
2023-08-30 00:10:40 +0200

    Add break-all characters in Card URLs

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import type { CardProps } from '../types';
 
 const Card = ({ header, footer, content, url, image }: CardProps) => {
   return (
-    <div className='rounded-3xl bg-secondary h-full w-full'>
+    <div className='rounded-3xl bg-secondary place-self-stretch h-full'>
       {image ? (
         <img
           src={image}
@@ -10,10 +10,17 @@ const Card = ({ header, footer, content, url, image }: CardProps) => {
           className='max-w-[260px] max-h-[260px] rounded-3xl'
         />
       ) : (
-        <div className='px-3 py-2 grid h-full grid-flow-row'>
+        <div className='px-3 py-2 grid h-full grid-flow-row gap-2'>
           <h2 className='text-md align-top text-primary/70'>{header}</h2>
-          <p className='text-xl self-end text-primary row-span-3 font-medium'>{content}</p>
+          <p className='text-xl self-end text-primary row-span-3 font-medium break-words'>
+            {content}
+          </p>
           <p className='text-md self-end text-primary font-medium'>{footer}</p>
+          {url && (
+            <a href={url} target='_blank' rel='noreferrer' className='break-all'>
+              {url}
+            </a>
+          )}
         </div>
       )}
     </div>
