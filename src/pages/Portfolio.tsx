f02629d114b4b1a533d7daea81536625490a15f4
Ibrahim Mammadov <zxyctn@gmail.com>
2024-06-01 14:02:46 +0200

    Add new CV

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -19,7 +19,7 @@ const Portfolio = () => {
   const clickHandler = (id: string) => {
     if (id === 'resume') {
       window.open(
-        'https://drive.google.com/file/d/1QXShjfgnfvk1mP6gARd5gvhuaNsHj-Na/view?usp=sharing',
+        'https://drive.google.com/file/d/1YTR_ONUIllLzGVQZfi6XR1ePjy_KR0W4/view?usp=sharing',
         '_blank'
       );
       return;
